76784dcf621e7624e5a90a60a2537827c99830ed
Valacor <valyrious.informatics@gmail.com>
2023-09-12 20:20:43 +0200

    Edit venv config

--- smarthomeweb_venv/Scripts/activate.bat
+++ smarthomeweb_venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Bx\Documents\_Pool\Lernsituationen\LS-SmartHomeWeb\smarthomeweb\smarthomeweb_venv
+set VIRTUAL_ENV=C:\Hackerspace\Source\SmarthomeWeb\smarthomeweb_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
